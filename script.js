d202709a31a6ebe9c29fa2738e23aa9a2f05aedd
rakesh <rakesh.joshi@meetingdecisions.com>
2022-08-18 09:48:40 +0530

    initial funtion to get search results

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+import { rapidApiKey } from './apikeys.js'
+
 const timer = document.querySelector('#time span')
 
 setInterval(() => {
 	timer.innerText = new Date().toLocaleString()
 }, 1000)
+
+
+
+function getSearchResults() {
+
+	fetch('https://genius-song-lyrics1.p.rapidapi.com/search?q=Alan%20Walker', {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': rapidApiKey,
+			'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com'
+		}
+	}).then(res => res.json())
+		.then(res => console.log(res))
+		.catch(err => console.error(err));
+}
